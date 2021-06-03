43a956b5a3711b838458a3ef195f43460dc70345
apan-ia <82051942+apan-ia@users.noreply.github.com>
2021-06-03 18:46:52 +0700

    Update updatePengingat.php

--- pengingat v2.0/updatePengingat.php
+++ pengingat v2.0/updatePengingat.php
@@ -4,16 +4,16 @@
 include('config.php');
 
 //get data dari form
-$id_kegiatan     = $_POST['id_kegiatan'];
+$id    = $_POST['id'];
 $bulan         = $_POST['bulan'];
 $kegiatan = $_POST['kegiatan'];
 $status       = $_POST['status'];
 
 //query update data ke dalam database berdasarkan ID
-$query = "UPDATE pengingat SET bulan = '$bulan', kegiatan = '$kegiatan', status = '$status' WHERE id_kegiatan = '$id_kegiatan'";
+$query = "UPDATE pengingat SET bulan = '$bulan', kegiatan = '$kegiatan', status = '$status' WHERE id = '$id'";
 
 //kondisi pengecekan apakah data berhasil diupdate atau tidak
-if($connection->query($query)) {
+if($conn->query($query)) {
     //redirect ke halaman index.php 
     header("location: pengingatAdmin.php");
 } else {
