9efaeca140ec4dc6f3a8bef381284f19de37b0c0
apan-ia <82051942+apan-ia@users.noreply.github.com>
2021-06-03 18:55:52 +0700

    Delete config.php

--- pengingat v2.0/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$conn = mysqli_connect("localhost","root","","rpl");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
- 
-?>
